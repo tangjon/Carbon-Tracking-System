4a93c969fa64bf5c7d70411f0f7cb0d371095604
tangj <jct19@sfu.ca>
2017-02-27 20:05:19 -0800

    implement car model class

---
+++ app/src/main/java/cmpt276/jade/carbontracker/model/Car.java
@@ -0,0 +1,71 @@
+package cmpt276.jade.carbontracker.model;
+
+/**
+ * Created by tangj on 2/27/2017.
+ */
+
+public class Car {
+    String make; // Manufacturer
+    String model;
+    String nickname;
+    int year;
+    int uCity;
+    int uHighway;
+    int carbonTailPipe;
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getuCity() {
+        return uCity;
+    }
+
+    public void setuCity(int uCity) {
+        this.uCity = uCity;
+    }
+
+    public int getuHighway() {
+        return uHighway;
+    }
+
+    public void setuHighway(int uHighway) {
+        this.uHighway = uHighway;
+    }
+
+    public int getCarbonTailPipe() {
+        return carbonTailPipe;
+    }
+
+    public void setCarbonTailPipe(int carbonTailPipe) {
+        this.carbonTailPipe = carbonTailPipe;
+    }
+}
