e7769289c4a8d083f8398cf1fc4c8ad4ea5f91f5
tangj <jct19@sfu.ca>
2017-02-28 00:40:31 -0800

    UI Display correct families of data
    Implements through spaghetti code. Needs refactoring and optimizing

--- app/src/main/java/cmpt276/jade/carbontracker/CarInfoActivity.java
+++ app/src/main/java/cmpt276/jade/carbontracker/CarInfoActivity.java
@@ -24,6 +24,11 @@ public class CarInfoActivity extends AppCompatActivity {
     // String of Models for Specific Car
     private List<String> modelList = new ArrayList<>();
 
+    // String of Years for Specific Car
+    private List<String> yearList = new ArrayList<>();
+
+    private String selectMake, selectModel, selectYear;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -32,17 +37,95 @@ public class CarInfoActivity extends AppCompatActivity {
         loadCarList();
 
         // Spinner for Manufacture
-        setUpMakeSpinner(R.id.spn_make);
+//        setUpMakeSpinner(R.id.spn_make);
+
+        setUpSpinner(R.id.spn_make);
 
 
     }
+    private void setUpSpinner(int spnID){
+        final Spinner spinner = (Spinner) findViewById(spnID);
+        ArrayAdapter<String> adapter = null;
+
+
+        switch (spnID){
+            case R.id.spn_make:
+                populateMakeList();
+                // Create an ArrayAdapter using the string array and a default spinner layout
+                adapter = new ArrayAdapter<String>(
+                        this, android.R.layout.simple_spinner_item, makeList);
+                spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+                        modelList.clear();
+                        selectMake = (String) spinner.getSelectedItem();
+                        setUpSpinner(R.id.spn_model);
+                    }
+                    public void onNothingSelected(AdapterView<?> parent) {
+                    }
+                });
+
+                break;
+            case R.id.spn_model:
+                populateModelList(selectMake);
+                // Create an ArrayAdapter using the string array and a default spinner layout
+                adapter = new ArrayAdapter<String>(
+                        this, android.R.layout.simple_spinner_item, modelList);
+                spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+                        yearList.clear();
+                        selectModel = (String) spinner.getSelectedItem();
+                        setUpSpinner(R.id.spn_year);
+                    }
+                    public void onNothingSelected(AdapterView<?> parent) {
+                    }
+                });
+                break;
+            case R.id.spn_year:
+                populateYearList();
+                // Create an ArrayAdapter using the string array and a default spinner layout
+                adapter = new ArrayAdapter<String>(
+                        this, android.R.layout.simple_spinner_item, yearList);
+                spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+                        yearList.clear();
+                        selectYear = (String) spinner.getSelectedItem();
+                    }
+                    public void onNothingSelected(AdapterView<?> parent) {
+                    }
+                });
+            default:
+                break;
+        }
+
+
+
+
+
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+    }
+
+    private void populateYearList() {
+        for (Car car: carList ) {
+            if(car.getMake().equals(selectMake) && car.getModel().equals(selectModel)){
+                yearList.add(Integer.toString(car.getYear()));
+            }
+        }
+        Toast.makeText(this, "" + yearList.toString(), Toast.LENGTH_SHORT).show();
+    }
+
+
     private void setUpMakeSpinner(int spnID) {
         populateMakeList();
-        Spinner spinner = (Spinner) findViewById(spnID);
+        final Spinner spinner = (Spinner) findViewById(spnID);
         // Listener
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
                 modelList.clear();
+                selectMake = (String) spinner.getSelectedItem();
+                Toast.makeText(CarInfoActivity.this, "" + selectMake, Toast.LENGTH_SHORT).show();
                 setModelSpinner(R.id.spn_model, makeList.get(pos));
             }
             public void onNothingSelected(AdapterView<?> parent) {
@@ -94,6 +177,7 @@ public class CarInfoActivity extends AppCompatActivity {
                 modelList.add(car.getModel());
             }
         }
+
     }
 
     private void loadCarList() {
