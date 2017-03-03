0fe616ff0da79b38fba8af3faf572b6be631a1ce
tangj <jct19@sfu.ca>
2017-03-03 00:04:02 -0800

    Add enum initial commit

--- app/src/main/java/cmpt276/jade/carbontracker/CarListActivity.java
+++ app/src/main/java/cmpt276/jade/carbontracker/CarListActivity.java
@@ -4,18 +4,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.List;
 
 import cmpt276.jade.carbontracker.adapter.CarListAdapter;
+import cmpt276.jade.carbontracker.model.Car;
 import cmpt276.jade.carbontracker.model.CarCollection;
 
 public class CarListActivity extends AppCompatActivity {
 
     //TODO TEMPORARY CAR COLLECTION STATIC SHOULD PLACED IN LOG CLASS
     public static CarCollection globCollection = new CarCollection();
+
+    private ListView lstView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,17 +28,33 @@ public class CarListActivity extends AppCompatActivity {
 
         setUpAddButton(R.id.btn_add_car);
 
-        //TODO String Array Argument to be implemented
         updateUI();
 
 
     }
 
+    private void setUpEditMode() {
+        lstView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Fetch Car Object
+                Car car = (Car) parent.getAdapter().getItem(position);
+
+
+            }
+        });
+    }
+
     private void updateUI() {
         List<String> stringList = globCollection.makeToStringList();
 
+
+        setUpListView();
+    }
+
+    private void setUpListView() {
         // Link widget
-        ListView lstView = (ListView) findViewById(R.id.lv_carList);
+        lstView = (ListView) findViewById(R.id.lv_carList);
         // Create an ArrayAdapter using the string array and a default spinner layout
         CarListAdapter adapter = new CarListAdapter(CarListActivity.this, globCollection.toList());
 //        ArrayAdapter<String> adapter = new ArrayAdapter<>(
@@ -43,6 +63,9 @@ public class CarListActivity extends AppCompatActivity {
 //        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         lstView.setAdapter(adapter);
+
+        // TODO Implement Edit Mode/Delete
+        setUpEditMode();
     }
 
     private void setUpAddButton(int btnID) {
---
+++ app/src/main/java/cmpt276/jade/carbontracker/utils/Mode.java
@@ -0,0 +1,9 @@
+package cmpt276.jade.carbontracker.utils;
+
+/**
+ * Created by tangj on 3/3/2017.
+ */
+
+public enum Mode {
+    ADD, EDIT, DELETE
+}
