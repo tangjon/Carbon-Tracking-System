40efc8b9b3218c759683239f5f1260b1909dce00
rcretu <rcretu@sfu.ca>
2017-03-12 16:41:18 -0700

    populate utilities lists with dummy data

--- app/src/main/java/cmpt276/jade/carbontracker/model/Utilities.java
+++ app/src/main/java/cmpt276/jade/carbontracker/model/Utilities.java
@@ -1,7 +1,13 @@
 package cmpt276.jade.carbontracker.model;
 
-public class Utilities {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Utilities implements Serializable {
     private int numResidents;
+    private List<Object> listBillElec;  // change <Object> to correct utility object when possible
+    private List<Object> listBillGas;
 
     Utilities() {
         loadData();
@@ -9,6 +15,9 @@ public class Utilities {
 
     private void loadData() {
         numResidents = 0;
+        listBillElec = new ArrayList<>();
+        listBillGas = new ArrayList<>();
+
     }
 
     public int getNumResidents() {
@@ -17,5 +26,21 @@ public class Utilities {
 
     public void setNumResidents(int numResidents) {
         if (numResidents > 0) this.numResidents = numResidents;
+    }
+
+    public List<Object> getListBillElec() {
+        return listBillElec;
+    }
+
+    public void setListBillElec(List<Object> listBillElec) {
+        this.listBillElec = listBillElec;
+    }
+
+    public List<Object> getListBillGas() {
+        return listBillGas;
+    }
+
+    public void setListBillGas(List<Object> listBillGas) {
+        this.listBillGas = listBillGas;
     }
 }
