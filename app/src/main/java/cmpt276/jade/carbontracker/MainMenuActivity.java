d43a5a7dfe030b1ddeaf19e0bc894283c03d1bea
tangj <jct19@sfu.ca>
2017-03-11 22:22:54 -0800

    Rename nav bar elements

--- app/src/main/java/cmpt276/jade/carbontracker/MainMenuActivity.java
+++ app/src/main/java/cmpt276/jade/carbontracker/MainMenuActivity.java
@@ -80,18 +80,18 @@ public class MainMenuActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        switch (id) {
+            case R.id.nav_overview:
+                // Handle the camera action
+                break;
+            case R.id.nav_journeys:
 
-        } else if (id == R.id.nav_slideshow) {
+                break;
+            case R.id.nav_utilities:
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+                break;
+            case R.id.nav_other:
+                break;
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
