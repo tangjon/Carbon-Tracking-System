d2f7ae36cd0577f58433fa2b3a46f5a247ca11ad
LJTANG <jct19@sfu.ca>
2017-04-05 14:43:41 -0700

    Hide nav bar for main menu

--- app/src/main/java/cmpt276/jade/carbontracker/Temp_Main_Acticity.java
+++ app/src/main/java/cmpt276/jade/carbontracker/Temp_Main_Acticity.java
@@ -24,7 +24,24 @@ public class Temp_Main_Acticity extends AppCompatActivity {
         setupUtilitiesBtn();
         setupAboutUsBtn();
         setupSettingsBtn();
+
+        hideSystemUI();
     }
+
+    private void hideSystemUI() {
+        View mDecorView = getWindow().getDecorView();
+        // Set the IMMERSIVE flag.
+        // Set the content to appear under the system bars so that the content
+        // doesn't resize when the system bars hide and show.
+        mDecorView.setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                | View.SYSTEM_UI_FLAG_IMMERSIVE);
+    }
+
 
     private void setupAboutUsBtn() {
         Button btn = (Button) findViewById(R.id.btn_about);
