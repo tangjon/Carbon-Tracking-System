e8bf97a61eef009b8d3063e6d1e2ef9aeac77a02
LJTANG <jct19@sfu.ca>
2017-04-05 15:01:04 -0700

    Android updated?

--- app/src/main/java/cmpt276/jade/carbontracker/AboutUsActivity.java
+++ app/src/main/java/cmpt276/jade/carbontracker/AboutUsActivity.java
@@ -2,15 +2,16 @@ package cmpt276.jade.carbontracker;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.method.LinkMovementMethod;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 public class AboutUsActivity extends AppCompatActivity {
 
-  public static Intent getIntent(Context ctx){
+  public static Intent getIntent(Context ctx) {
     Intent intent = new Intent(ctx, AboutUsActivity.class);
     return intent;
   }
@@ -22,7 +23,22 @@ public class AboutUsActivity extends AppCompatActivity {
     setContentView(R.layout.activity_about_us);
     getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     setUpLinks();
+    hideSystemUI();
 
+  }
+
+  private void hideSystemUI() {
+    View mDecorView = getWindow().getDecorView();
+    // Set the IMMERSIVE flag.
+    // Set the content to appear under the system bars so that the content
+    // doesn't resize when the system bars hide and show.
+    mDecorView.setSystemUiVisibility(
+        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+//            | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+            | View.SYSTEM_UI_FLAG_IMMERSIVE);
   }
 
   private void setUpLinks() {
--- app/src/main/java/cmpt276/jade/carbontracker/Temp_Main_Acticity.java
+++ app/src/main/java/cmpt276/jade/carbontracker/Temp_Main_Acticity.java
@@ -38,7 +38,7 @@ public class Temp_Main_Acticity extends AppCompatActivity {
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-                | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+//                | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
                 | View.SYSTEM_UI_FLAG_IMMERSIVE);
     }
 
