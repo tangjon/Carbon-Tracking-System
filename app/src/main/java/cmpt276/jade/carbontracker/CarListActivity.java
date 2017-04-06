673098fa889f4624a6940cdd81506c5d0fc3967e
tangj <jct19@sfu.ca>
2017-04-06 00:20:16 -0700

    Testing immersion sticky

--- app/src/main/java/cmpt276/jade/carbontracker/CarListActivity.java
+++ app/src/main/java/cmpt276/jade/carbontracker/CarListActivity.java
@@ -78,11 +78,9 @@ public class CarListActivity extends AppCompatActivity {
         // doesn't resize when the system bars hide and show.
 
         int uiOptions =
-                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN;
 
         mDecorView.setSystemUiVisibility(uiOptions);
     }
