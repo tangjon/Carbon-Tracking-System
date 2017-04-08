437c56f022cf9a2f0faf7e724597985b92d4add8
rcretu <rcretu@sfu.ca>
2017-04-07 22:33:47 -0700

    Restore non-cancelable date picker in graph
    Didn't mean to revert this

--- app/src/main/java/cmpt276/jade/carbontracker/CarbonFootprintActivity.java
+++ app/src/main/java/cmpt276/jade/carbontracker/CarbonFootprintActivity.java
@@ -170,6 +170,7 @@ public class CarbonFootprintActivity extends AppCompatActivity {
                 hideSystemUI();
             }
         }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+        dialog.setCancelable(false);
     }
 
     private Date currentDate() {
